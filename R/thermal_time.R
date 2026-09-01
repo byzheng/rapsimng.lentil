979a7cc8a92c3478d416ed29bcf2b9637d3dee96
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-09-01 15:18:06 +1000

    Update documentation links for thermal time and vernalisation time functions

--- R/thermal_time.R
+++ R/thermal_time.R
@@ -4,7 +4,7 @@
 #' @details 
 #' The daily thermal time is calculated by interpolating the effective thermal time at three hour intervals between the daily minimum and maximum temperatures, and then averaging these values to get the daily thermal time.
 #' For more information about the lentil phenology model, see the 
-#' [APSIM Next Generation lentil Documentation](https://notes.apsimng.bangyou.me/docs/Plants/lentil/Phenology/ThermalTime.html).
+#' [APSIM Next Generation Lentil Documentation](https://notes.apsimng.bangyou.me/docs/Plants/Lentil/Phenology/ThermalTime.html).
 #' 
 #' @param maxt The maximum temperature
 #' @param mint The minimum temperature
