20fff85a8206c04896ee130f401dd30393d68058
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-09-14 15:20:25 +1000

    Add return value description for lentil options manager

--- R/options.R
+++ R/options.R
@@ -22,6 +22,7 @@
 #' @export
 #' @importFrom optree create_options_manager
 #' @importFrom optree v_xypair
+#' @return An option manager object for configuring lentil model parameters.
 #' @examples
 #' # Get default thermal time cardinal temperatures
 #' lentil$get("phenology.thermal_time.x")
